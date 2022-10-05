ef982b6270f880934c5ac8bc109054cd71090459
Antonin Boyon <antonin.boyon@gmail.com>
2022-10-05 17:04:52 +0200

    new function
    confirmation function

--- WpfApp1/Order.cs
+++ WpfApp1/Order.cs
@@ -4,28 +4,49 @@ namespace WpfApp1 // Note: actual namespace depends on the project name.
 {
     internal class Order
     {
-        protected integer orderId { get; set; }
-        protected date hour { get; set; }
+        protected int orderId { get; set; }
+        protected int clientId { get; set; }
+        protected DateTime hour { get; set; }
         protected string clientName { get; set; }
         protected string assistantName { get; set; }
         protected string orderState { get; set; }
         protected List<Product> ListProducts { get => listProducts; set => listProducts = value; }
-        
-        Order (integer orderId, date hour, string clientName, string assistantName, string orderState, List<Product> ListProducts) {
+
+        Order (int orderId, date hour, string clientName, int idClient, string assistantName, string orderState, List<Product> ListProducts) {
             this.orderId = orderId;
             this.hour = hour;
             this.clientName = clientName;
             this.assistantName = assistantName;
             this.orderState = orderState;
             this.ListProducts = ListProducts;
+            this.clientId = idClient;
         }
 
-        Order (integer orderId, date hour, string clientName, string assistantName, string orderState) {
+        Order (int orderId, date hour, string clientName, int idClient, string assistantName, string orderState) {
             this.orderId = orderId;
             this.hour = hour;
             this.clientName = clientName;
             this.assistantName = assistantName;
             this.orderState = orderState;
+            this.clientId = idClient;
         }
+
+        Boolean clientConfirmation () {
+            return true;
+        }
+
+        List<Pizza> kitchenConfirmation () {
+            return new List<Pizza>();
+        }
+
+        Address deliveryConfirmation (int idClient) {
+            return default;
+        }
+
+        Order assistantConfirmation () {
+            return this;
+        }
+
+
     }
 }
