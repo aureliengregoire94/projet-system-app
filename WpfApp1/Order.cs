edb17eb529afd21558c88d002986485d686df605
Antonin Boyon <antonin.boyon@gmail.com>
2022-10-05 17:19:26 +0200

    Revert "yes"
    This reverts commit 2f0d04675103017a25da4f32735edc3beb867cb6.

--- WpfApp1/Order.cs
+++ WpfApp1/Order.cs
@@ -12,23 +12,23 @@ namespace WpfApp1 // Note: actual namespace depends on the project name.
         protected string orderState { get; set; }
         protected List<Product> ListProducts { get => listProducts; set => listProducts = value; }
 
-        Order (int orderId, DateTime hour, string clientName, int clientId, string assistantName, string orderState, List<Product> ListProducts) {
+        Order (int orderId, date hour, string clientName, int idClient, string assistantName, string orderState, List<Product> ListProducts) {
             this.orderId = orderId;
             this.hour = hour;
             this.clientName = clientName;
             this.assistantName = assistantName;
             this.orderState = orderState;
             this.ListProducts = ListProducts;
-            this.clientId = clientId;
+            this.clientId = idClient;
         }
 
-        Order (int orderId, DateTime hour, string clientName, int clientId, string assistantName, string orderState) {
+        Order (int orderId, date hour, string clientName, int idClient, string assistantName, string orderState) {
             this.orderId = orderId;
             this.hour = hour;
             this.clientName = clientName;
             this.assistantName = assistantName;
             this.orderState = orderState;
-            this.clientId = clientId;
+            this.clientId = idClient;
         }
 
         Boolean clientConfirmation () {
