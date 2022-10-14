5820ca42394078c818b55ef38fec303a9b2f7a35
aureliengregoire94 <60916671+tributZ@users.noreply.github.com>
2022-10-14 17:56:06 +0200

    putain de sa race

---
+++ WpfApp1/Keydownhandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    internal class Keydownhandler
+    {
+        private object sender;
+        private RoutedEventArgs e;
+        private TextBox txt;
+
+        public Keydownhandler(object sender, RoutedEventArgs e, TextBox txt)
+        {
+            this.sender = sender;
+            this.e = e;
+            this.txt = txt;
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,30 +23,96 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void validation_Click(object sender, RoutedEventArgs e)
+        
+        private void Validation_Click(object sender, RoutedEventArgs e)
         {
-            if(Cli.IsChecked == true)
+
+            if (Cli.IsChecked == true)
             {
+
+
                 MessageBox.Show("Enter the order ID");
+
+                TextBox txt = new TextBox();
+                txt.Name = "new_input";
+                Thickness margin = txt.Margin;
+                margin.Left = 280;
+                margin.Top = 300;
+                margin.Bottom = 194;
+                txt.Width = 170;
+                txt.Height = 20;
+
+                txt.KeyDown += new KeyEventHandler(PressEnter);
+
+                interface1.Children.Add(txt);
+
             }
-            else if(Own.IsChecked == true)
+            else if (Own.IsChecked == true)
             {
                 MessageBox.Show("Enter the owner code");
+
+                TextBox txt = new TextBox();
+                txt.Name = "new_input";
+                Thickness margin = txt.Margin;
+                margin.Left = 280;
+                margin.Top = 300;
+                margin.Bottom = 194;
+                txt.Width = 170;
+                txt.Height = 20;
+
+                txt.KeyDown += new KeyEventHandler(PressEnter);
+
+                interface1.Children.Add(txt);
+
             }
-            else if(Emp.IsChecked == true)
+            else if (Emp.IsChecked == true)
             {
                 MessageBox.Show("Enter the employee Code");
+
+                TextBox txt = new TextBox();
+                txt.Name = "new_input";
+                Thickness margin = txt.Margin;
+                margin.Left = 280;
+                margin.Top = 300;
+                margin.Bottom = 194;
+                txt.Width = 170;
+                txt.Height = 20;
+
+                txt.KeyDown += new KeyEventHandler(PressEnter);
+
+                interface1.Children.Add(txt);
             }
-            else if(Deliv.IsChecked == true)
+            else if (Deliv.IsChecked == true)
             {
                 MessageBox.Show("Enter the Delivery Code");
-            }
 
+                TextBox txt = new TextBox();
+                txt.Name = "new_input";
+                Thickness margin = txt.Margin;
+                margin.Left = 280;
+                margin.Top = 300;
+                margin.Bottom = 194;
+                txt.Width = 170;
+                txt.Height = 20;
+
+                txt.KeyDown += new KeyEventHandler(PressEnter);
+
+                interface1.Children.Add(txt);
+            }
+        }
+
+        private void PressEnter(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                TextBox txt = (TextBox)sender;
+                MessageBox.Show(txt.Text);
+            }
         }
     }
 }
